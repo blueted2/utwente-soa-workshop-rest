c00ea6253ca9177a43d5cae7d53ee90a543ec766
Gregory Holder <gregory.holder76@gmail.com>
2023-03-06 13:44:44 +0100

    Add end points for adding and removing jobs

--- src/main/java/nl/cofano/schedulerapp/rest/ScheduleItemRestController.java
+++ src/main/java/nl/cofano/schedulerapp/rest/ScheduleItemRestController.java
@@ -4,6 +4,7 @@ import nl.cofano.schedulerapp.scheduler.ScheduleItem;
 import nl.cofano.schedulerapp.scheduler.ScheduleItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.time.Instant;
 import java.util.Collection;
 
 @RestController
@@ -26,4 +27,26 @@ public class ScheduleItemRestController {
     public Collection<ScheduleItem> getAll(){
         return service.getItems();
     }
+
+    @RequestMapping(
+        path="/item",
+        method = RequestMethod.POST
+    )
+    public ScheduleItem addItem(@RequestBody ScheduleItem scheduleItem) {
+        // if last executed is not set, set it to "now"
+        if (scheduleItem.getLastExecuted() == null) {
+            scheduleItem.setLastExecuted(Instant.now());
+        }
+        return service.createItem(scheduleItem);
+    }
+
+    @RequestMapping(
+        path="/item/{id}",
+        method = RequestMethod.DELETE
+    )
+    public void deleteItem(@PathVariable Integer id){
+        service.deleteItem(id);
+    }
+
+
 }

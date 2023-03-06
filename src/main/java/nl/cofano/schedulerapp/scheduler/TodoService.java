c42bd3e2b11de9347b43b15e0d071d20e6ee5ef7
Gregory Holder <gregory.holder76@gmail.com>
2023-03-06 13:39:05 +0100

    Call add todo endpoint for jobs

--- src/main/java/nl/cofano/schedulerapp/scheduler/TodoService.java
+++ src/main/java/nl/cofano/schedulerapp/scheduler/TodoService.java
@@ -2,7 +2,10 @@ package nl.cofano.schedulerapp.scheduler;
 
 import nl.cofano.schedulerapp.exceptions.TodoCreateException;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 
 /**
@@ -10,10 +13,35 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TodoService {
+
+    public static class TodoRepresentation {
+        public Integer assignee;
+        public String description;
+        public Integer id;
+        public boolean markedAsFinished;
+
+        public TodoRepresentation() {
+        }
+
+        public TodoRepresentation(Integer assignee, String description) {
+            this.assignee = assignee;
+            this.description = description;
+            this.id = 0;
+            this.markedAsFinished = false;
+        }
+    }
+
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(TodoService.class);
 
+    @Autowired private RestTemplateBuilder restTemplateBuilder;
     public void createTodo(String description, Integer assignee) throws TodoCreateException{
         // Add an webservice call for creating an item to the todo-app
-        log.info("This should be replaced by todo creation code, with values: description={}, assignee={}", description, assignee);
+        String url = "http://localhost:8081/todo";
+        
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        TodoRepresentation todoRepresentation = new TodoRepresentation(assignee, description);
+
+        log.info("Adding todo with values: description={}, assignee={}", description, assignee);
+        todoRepresentation = restTemplate.postForObject(url, todoRepresentation, TodoRepresentation.class);
     }
 }
